2b87f9999b306765c6e40d7890aff71d5360274e
Minkyu <minkyu070602@gmail.com>
2025-02-24 18:26:48 -0800

    Add teleop heading lock

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drivetrain.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriver;
+import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -11,11 +13,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.util.DcMotorGroup;
 
 public class Drivetrain {
+    @Config
+    public static class headingPIDF {
+        public static double p = 2.2, i = 0, d = 0, f = 0;
+    }
+
     private DcMotorEx _frontLeftMotor, _frontRightMotor, _backLeftMotor, _backRightMotor;
     private DcMotorGroup dtMotors;
     static public GoBildaPinpointDriver odo;
     private double maxPower;
     private boolean isFieldCentricDriveEnabled = true;
+    private boolean isHeadingLockEnabled = true;
+    public double targetHeading;
+    private boolean isTurning = false;
+    private PIDFController headingPIDFController;
 
     public Drivetrain(HardwareMap hwMap) {
         _frontLeftMotor = hwMap.get(DcMotorEx.class, "leftFront");
@@ -35,16 +46,37 @@ public class Drivetrain {
         dtMotors = new DcMotorGroup(_frontLeftMotor, _frontRightMotor, _backLeftMotor, _backRightMotor);
         dtMotors.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         dtMotors.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        headingPIDFController = new PIDFController(headingPIDF.p, headingPIDF.i, headingPIDF.d, headingPIDF.f);
+        targetHeading = -odo.getHeading();
+    }
+
+    private double smoothControl(double value) {
+        return 0.5 * Math.tan(1.107 * value);
     }
 
     public void drive(double strafeSpeed,
                       double forwardSpeed,
                       double turnSpeed) {
         odo.update();
+        headingPIDFController.setPIDF(headingPIDF.p, headingPIDF.i, headingPIDF.d, headingPIDF.f);
+
+        strafeSpeed = smoothControl(strafeSpeed);
+        forwardSpeed = smoothControl(forwardSpeed);
+        turnSpeed = smoothControl(turnSpeed);
 
         double heading;
         if (!isFieldCentricDriveEnabled) heading = 0;
         else heading = -odo.getHeading();
+
+        if (turnSpeed != 0) isTurning = true;
+        else if (isTurning && Math.abs(odo.getHeadingVelocity()) < 0.02) {
+            isTurning = false;
+            targetHeading = heading;
+        }
+
+        if (!isTurning && isHeadingLockEnabled)
+            turnSpeed = headingPIDFController.calculate(heading, targetHeading);
 
         forwardSpeed *= -1;
 
@@ -62,6 +94,11 @@ public class Drivetrain {
         _backRightMotor.setPower((ySpeed + xSpeed - turnSpeed) / max * maxPower);
     }
 
+    public void toggleHeadingLock() {
+        isHeadingLockEnabled = !isHeadingLockEnabled;
+        targetHeading = -odo.getHeading();
+    }
+
     public void resetImu() {
         odo.resetPosAndIMU();
     }
@@ -76,5 +113,13 @@ public class Drivetrain {
 
     public Pose2D getPosition() {
         return odo.getPosition();
+    }
+
+    public boolean isFieldCentricDriveEnabled() {
+        return isFieldCentricDriveEnabled;
+    }
+
+    public boolean isHeadingLockEnabled() {
+        return isHeadingLockEnabled;
     }
 }
